0e6ac4baa5887934f607a998018579b740e296ac
Dipen Chauhan <get.dipen@gmail.com>
2013-10-28 02:08:50 -0700

    Added discrepancy function

--- Project1/gridworld.rb
+++ Project1/gridworld.rb
@@ -26,12 +26,34 @@ def init
 	@probability_e_given_X = [0.6561, 0.0729, 0.0081, 0.0009, 0.0001]
 
 	# f[i] = P(X0 = i)
-	@f = Array.new(@NUMBER_OF_LOCATIONS, 1/@NUMBER_OF_LOCATIONS)
+	@f = Matrix.column_vector(Array.new(@NUMBER_OF_LOCATIONS, 1.0/@NUMBER_OF_LOCATIONS))
 end
 
 
-def calc
+# Observation to binary
+# eg. when e = NW, North and West have obstacles
+# 		result = 1010
+# result is in the format <NSWE>
+# 1 - obstacle; 0 - no obstacle
+def otob(observation)
 
+end
+
+# Calculate probabilities
+def calc(observation)
+	
+end
+
+# Discrepancy
+# number of bits that differ in value between ei and the corresponding value for <NSWE> for the square.
+def discrepancy(a, b)
+	case
+		when a.empty? then b.length
+		when b.empty? then a.length
+		else [(a[0] == b[0] ? 0 : 1) + discrepancy(a[1..-1], b[1..-1]),
+				1 + discrepancy(a[1..-1], b),
+				1 + discrepancy(a, b[1..-1])].min
+	end
 end
 
 
@@ -46,10 +68,15 @@ print "+---+---+---+---+---+\n\n"
 
 print "Observations: "
 print ARGV.to_s
+print "\n\n"
+
+init
+
+
+
+# @r.to_a.each {|r| puts r.inspect}
 
 # For each argument
 ARGV.each do|a|
 
 end
-
-init
