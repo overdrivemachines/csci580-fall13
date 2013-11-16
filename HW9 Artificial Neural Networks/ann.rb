bcea3ad38d2f83297c09c7f2be6e29b9c70cac4e
Dipen Chauhan <get.dipen@gmail.com>
2013-11-16 04:42:43 -0800

    Created ann.rb

---
+++ HW9 Artificial Neural Networks/ann.rb
@@ -0,0 +1,11 @@
+#////////////////////////////////////////////////////#
+# Title: CSCI 580 - Homework 9 - Artificial Neural Networks
+# Filename: ann.rb
+# Date Created: 11/16/2013 
+# Author: Dipen Chauhan
+# Copyright Dipen Chauhan
+#////////////////////////////////////////////////////#
+
+def init
+
+end
