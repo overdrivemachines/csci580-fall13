0e2b69a8bfbcc64fffb948f9e3343a7e6252e0e6
Dipen Chauhan <get.dipen@gmail.com>
2013-11-19 14:23:01 -0800

    Added outputs for different values for N

--- HW9 Artificial Neural Networks/ann.rb
+++ HW9 Artificial Neural Networks/ann.rb
@@ -50,18 +50,15 @@ def calc
 	@a[2] = @x12
 
 	# Step 2
-	@in[3] = @a[0] * @w[0][3] + @a[1] * @w[1][3] + @a[2] * @w[2][4]
-	print "in[3]: "
-	print @in[3]
-	print "\n"
-	@a[3] = 1 / (1 + Math.exp(-@in[3]))
-	print "a[3]: "
-	print @a[3]
-	print "\n"
+	#layer l = 2
+		# Step 3
+	@in[3] = (@a[0] * @w[0][3]) + (@a[1] * @w[1][3]) + (@a[2] * @w[2][3])
+	@a[3] = 1 / (1 + Math.exp(-@in[3]))	
 	@in[4] = @a[0]*@w[0][4] + @a[1]*@w[1][4] + @a[2]*@w[2][4]
 	@a[4] = 1 / (1 + Math.exp(-@in[4]))
 
-	# Step 3
+	#layer l = 3
+		# Step 3
 	@in[5] = @a[0]*@w[0][5] + @a[3]*@w[3][5] + @a[4]*@w[4][5]
 	@a[5] = 1 / (1 + Math.exp(-@in[5]))
 	@in[6] = @a[0]*@w[0][6] + @a[3]*@w[3][6] + @a[4]*@w[4][6]
@@ -72,9 +69,12 @@ def calc
 	@error[6] = @a[6] * (1 - @a[6]) * (@y6 - @a[6])
 
 	# Step 5
-	# Step 6
+	# layer l = 2
+		# Step 6
 	@error[3] = @a[3] * (1 - @a[3]) * (@error[5] * @w[3][5] + @error[6] * @w[3][6])
 	@error[4] = @a[4] * (1 - @a[4]) * (@error[5] * @w[4][5] + @error[6] * @w[4][6])
+	# layer l = 1
+		# Step 6
 	@error[1] = @a[1] * (1 - @a[1]) * (@error[3] * @w[1][3] + @error[4] * @w[1][4])
 	@error[2] = @a[2] * (1 - @a[2]) * (@error[3] * @w[2][3] + @error[4] * @w[2][4])
 
@@ -95,6 +95,7 @@ def calc
 	@w[4][6] = @w[4][6] + @alpha * @a[4] * @error[6]
 end
 
+
 print "===============================\n"
 print "HW9: Artificial Neural Networks\n"
 print "===============================\n"
@@ -103,14 +104,15 @@ print "by Dipen Chauhan\n\n"
 init
 
 if ARGV.size == 0
-	n = 1
+	n = 100
+
 	print "N = %d\n" % [n]
 	init
 	for i in 1..n
 		calc
 	end
 	
-	print "Output a_j\n"
+	print "\nOutput a_j:\n"
 	for i in 1..(@a.size - 1)
 		print i
 		print ": "
@@ -118,13 +120,86 @@ if ARGV.size == 0
 		print "\n"
 	end
 
-	print "Errors delta_j: \n"	
+	print "\nErrors delta_j: \n"	
 	for i in 1..(@error.size - 1)
 		print i
 		print ": "
 		print @error[i]
 		print "\n"
 	end
+
+	print "\nWeights w:\n"
+	for i in 0..(@w[0].size - 1)
+		print @w[i]
+		print "\n"
+	end
+
+	print "===============================\n"
+
+
+	n = 1000
+
+	print "N = %d\n" % [n]
+	init
+	for i in 1..n
+		calc
+	end
+	
+	print "\nOutput a_j:\n"
+	for i in 1..(@a.size - 1)
+		print i
+		print ": "
+		print @a[i]
+		print "\n"
+	end
+
+	print "\nErrors delta_j: \n"	
+	for i in 1..(@error.size - 1)
+		print i
+		print ": "
+		print @error[i]
+		print "\n"
+	end
+
+	print "\nWeights w:\n"
+	for i in 0..(@w[0].size - 1)
+		print @w[i]
+		print "\n"
+	end
+
+	print "===============================\n"
+
+	n = 10000
+
+	print "N = %d\n" % [n]
+	init
+	for i in 1..n
+		calc
+	end
+	
+	print "\nOutput a_j:\n"
+	for i in 1..(@a.size - 1)
+		print i
+		print ": "
+		print @a[i]
+		print "\n"
+	end
+
+	print "\nErrors delta_j: \n"	
+	for i in 1..(@error.size - 1)
+		print i
+		print ": "
+		print @error[i]
+		print "\n"
+	end
+
+	print "\nWeights w:\n"
+	for i in 0..(@w[0].size - 1)
+		print @w[i]
+		print "\n"
+	end
+
+	print "===============================\n"
 
 	# print "error5 = %f\n" % [@error[5]]
 	# print "error6 = %f\n" % [@error[6]]
