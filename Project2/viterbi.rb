6cfd2ea58e3e9e76a8c43cc23d002483a6afa86e
Dipen Chauhan <get.dipen@gmail.com>
2013-12-13 12:33:02 -0800

    Initial commit

--- Project2/viterbi.rb
+++ Project2/viterbi.rb
@@ -1,11 +1,11 @@
 #////////////////////////////////////////////////////#
 # Title: CSCI 580 Project 2
-# Filename: em.rb
+# Filename: viterbi.rb
 # Date Created: 12/13/2013 
 # Author: Dipen Chauhan
 # Description:
 # Syntax for arguments
-# 	em
+# 	viterbi
 # This program generates:
 #
 # Program looks for an input file that contains values
